c3920341c7cf13f9d4f9af870d6a79066e36e18b
kingangelTOT <kingangela.258@163.com>
2015-11-16 01:03:13 +0800

    update

--- src/main/java/junyan/cucumber/RunCucumber.java
+++ src/main/java/junyan/cucumber/RunCucumber.java
@@ -3,20 +3,28 @@ package junyan.cucumber;
 import cucumber.api.cli.Main;
 import junyan.cucumber.support.util.CucumberReportMonitor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by kingangeltot on 15/11/4.
  */
 public class RunCucumber{
-    public static void puts(Object object){
-        System.out.println(object);
-    }
+    private static final String[] BASE_PARAMS = {"-p","json:cucumber-reports/cucumber.json", "-g", "junyan/cucumber/step_definitions"};
     public static void main(String[] args){
-        String[] params = {"-p","json:cucumber-reports/cucumber.json", "-g", "junyan/cucumber/step_definitions",
-                "/Users/kingangelTOT/Downloads/features", "-t", "@interface"};
-        String[] help = {"-p","pretty","-g", "junyan/cucumber/step_definitions", "--help"};
-        byte status = create(params);
+//        String[] help = {"-p","pretty","-g", "junyan/cucumber/step_definitions", "--help"};
+//        String[] test = {"/Users/kingangelTOT/Downloads/features", "-t", "@account"};
+        byte status = create(addParams(args));
         CucumberReportMonitor.create(new String[]{"-f", "cucumber-reports", "-o", "cucumber-reports", "-n"});
         System.exit(status);
+    }
+
+    public static String[] addParams(String[] args){
+        List<String> base = new ArrayList<>();
+        base.addAll(Arrays.asList(BASE_PARAMS));
+        base.addAll(Arrays.asList(args));
+        return base.toArray(new String[base.size()]);
     }
 
     public static byte create(String[] args){
